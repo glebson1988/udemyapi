6d4f31f3842af4ffe3bf80681e1ff1bbbf3032af
glebson1988 <glebson1988@yandex.ru>
2021-09-27 17:36:30 +0200

    adding article model, migration, spec

---
+++ spec/factories/articles.rb
@@ -0,0 +1,7 @@
+FactoryBot.define do
+  factory :article do
+    sequence(:title) { |n| "Some title#{n}" }
+    content { 'Sample content' }
+    sequence(:slug) { |n| "slug#{n}" }
+  end
+end
---
+++ spec/models/article_spec.rb
@@ -0,0 +1,40 @@
+require 'rails_helper'
+
+RSpec.describe Article, type: :model do
+  describe '#validations' do
+    let(:article) { build :article }
+
+    it 'tests that factory is valid' do
+      expect(article).to be_valid
+      article.save!
+      second_article = build(:article)
+      expect(second_article).to be_valid
+    end
+
+    it 'has an invalid title' do
+      article.title = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:title]).to include("can't be blank")
+    end
+
+    it 'has an invalid content' do
+      article.content = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:content]).to include("can't be blank")
+    end
+
+    it 'has an invalid slug' do
+      article.slug = ''
+      expect(article).not_to be_valid
+      expect(article.errors[:slug]).to include("can't be blank")
+    end
+
+    it 'validates uniqueness of the slug' do
+      third_article = create(:article)
+      expect(third_article).to be_valid
+      fourth_article = build(:article, slug: third_article.slug)
+      expect(fourth_article).not_to be_valid
+      expect(fourth_article.errors[:slug]).to include('has already been taken')
+    end
+  end
+end
